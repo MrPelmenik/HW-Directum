d0f8ae02fdddf1c5674eef2313436b975584e96e
MrPelmenik <MrPelmenik@yandex.ru>
2024-09-23 00:31:58 +0400

    Добавил правильный гитигнор v2

---
+++ HWForCountingPeople/WorkerFolder/Worker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWForCountingPeople.Worker
+{
+
+    public class OfficeWorker : IWorker
+    {
+
+        public string name { get; set; }
+        public string workPlace { get;}
+        public int salary { get; set; }
+        public OfficeWorker (string name, int salary) 
+        { 
+            this.name = name;
+            workPlace = "Office";
+            this.salary = salary;
+        }
+    }
+
+    public class ProductWorker : IWorker
+    {
+        public string name { get; set; }
+        public string workPlace { get;}
+        public int salary { get; set; }
+        public ProductWorker(string name, int salary)
+        { 
+            this.name = name;
+            workPlace = "Product";
+            this.salary = salary;
+        }
+    }
+
+}
